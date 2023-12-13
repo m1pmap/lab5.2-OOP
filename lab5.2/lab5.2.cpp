333a7a433ac7a53e4732974b97b174d8a5817a92
m1pmap <dok_koks@mail.ru>
2023-12-13 23:18:11 +0300

    Добавьте файлы проекта.

---
+++ lab5.2/lab5.2.cpp
@@ -0,0 +1,24 @@
+﻿#include<iostream>
+#include<string>
+#include<deque>
+#include<algorithm>
+
+using namespace std;
+
+
+struct Compare {
+    bool operator()(string a, string b)
+    {
+        if (a.size() != b.size())
+            return a.size() > b.size();
+        else
+            return a < b;
+    }
+};
+
+int main() {
+    deque<string> deque = { "apple", "cherry", "banana", "date", "elderberry" };
+    sort(deque.begin(), deque.end(), Compare());
+    for (auto i : deque)
+        cout << (i) << " ";
+}
