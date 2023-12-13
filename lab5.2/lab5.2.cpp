84b3a7d02932f03edee5b09bf5ca299e6318ab66
m1pmap <dok_koks@mail.ru>
2023-12-13 23:21:03 +0300

    task completed

--- lab5.2/lab5.2.cpp
+++ lab5.2/lab5.2.cpp
@@ -1,4 +1,10 @@
-﻿#include<iostream>
+﻿//Дан дек D, элементами которого являются английские слова.
+//Отсортировать его элементы по убыванию их длин, а элементы 
+//одинаковой длины — в алфавитном порядке.Использовать
+//единственный вызов алгоритма sort с параметром — функциональным
+//объектом, включающим как сравнение строк, так и сравнение их длин.
+
+#include<iostream>
 #include<string>
 #include<deque>
 #include<algorithm>
@@ -7,17 +13,22 @@ using namespace std;
 
 
 struct Compare {
-    bool operator()(string a, string b)
+    bool operator()(string firstStr, string secondStr)
     {
-        if (a.size() != b.size())
-            return a.size() > b.size();
+        if (firstStr.size() != secondStr.size())
+            return firstStr.size() > secondStr.size();
         else
-            return a < b;
+            return firstStr < secondStr;
     }
 };
 
 int main() {
     deque<string> deque = { "apple", "cherry", "banana", "date", "elderberry" };
+    cout << "deque: ";
+    for (auto i : deque)
+        cout << (i) << " ";
+
+    cout << "\nsort: ";
     sort(deque.begin(), deque.end(), Compare());
     for (auto i : deque)
         cout << (i) << " ";
